5dd5b2f5f65730854dcb90d780c150c7928d4772
David Pam <niratpam@gmail.com>
2021-05-15 01:52:47 +0100

    Replacing Pages (Instead of Pushing)

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -43,10 +43,10 @@ class MainDrawer extends StatelessWidget {
           ),
           SizedBox(height: 20),
           buildListTile('Meal', Icons.restaurant, () {
-            Navigator.of(context).pushNamed('/');
+            Navigator.of(context).pushReplacementNamed('/');
           }),
           buildListTile('Filters', Icons.settings, () {
-            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
           }),
         ],
       ),
